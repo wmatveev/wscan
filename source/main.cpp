8509a2649ad7661ccc73f243dfae649d87023511
wmatveev <matveev@matller.com>
2024-07-03 20:55:41 +0300

    testing

--- source/main.cpp
+++ source/main.cpp
@@ -7,27 +7,7 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-//    ITcpServer  *server = new TcpServer();
-
-    int ms = 500;
-
-    TrafficLightController *m_tl = new TrafficLightController;
-
-    while (1)
-    {
-        m_tl->TurnOffAllTheLights();
-        m_tl->BlueLight();
-        std::this_thread::sleep_for(std::chrono::milliseconds(ms));
-        m_tl->TurnOffAllTheLights();
-        m_tl->YellowLight();
-        std::this_thread::sleep_for(std::chrono::milliseconds(ms));
-        m_tl->TurnOffAllTheLights();
-        m_tl->GreenLight();
-        std::this_thread::sleep_for(std::chrono::milliseconds(ms));
-        m_tl->TurnOffAllTheLights();
-        m_tl->RedLight();
-        std::this_thread::sleep_for(std::chrono::milliseconds(ms));
-    }
+    ITcpServer  *server = new TcpServer();
 
     return a.exec();
 }
