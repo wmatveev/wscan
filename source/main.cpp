a81a2e2b73f0c982b87459c3f3a8f1ee7f94eb70
wmatveev <matveev@matller.com>
2024-07-03 18:54:46 +0300

    testing

--- source/main.cpp
+++ source/main.cpp
@@ -1,13 +1,27 @@
 #include <QCoreApplication>
 #include "TcpServer.hpp"
 
-#include "LaserController.hpp"
+#include "TrafficLightController.hpp"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    ITcpServer  *server = new TcpServer();
+//    ITcpServer  *server = new TcpServer();
+
+    TrafficLightController *m_tl = new TrafficLightController;
+
+    while (1)
+    {
+        m_tl->BlueLight();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        m_tl->YellowLight();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        m_tl->RedLight();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        m_tl->GreenLight();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
 
     return a.exec();
 }
