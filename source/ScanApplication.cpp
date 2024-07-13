1773e0b8c659683abb320366063a6da1524530d1
wmatveev <matveev@matller.com>
2024-07-13 10:38:04 +0300

    testing polling the first port

--- source/ScanApplication.cpp
+++ source/ScanApplication.cpp
@@ -7,6 +7,8 @@
 #include "PortPolling.hpp"
 #include "ScannerController.hpp"
 #include "TrafficLightController.hpp"
+#include "ITcpServer.hpp"
+#include "TcpServer.hpp"
 
 ScanApplication::ScanApplication(QObject *parent)
         : QObject(parent),
@@ -17,32 +19,32 @@ ScanApplication::ScanApplication(QObject *parent)
 
 void ScanApplication::RunApplication()
 {
-//    ITcpServer  *server = new TcpServer();
+    ITcpServer  *server = new TcpServer();
 
-    auto *portPolling = new PortPolling();
-    auto *timer = new QTimer(this);
-    auto *scannerController = new ScannerController();
-    auto *trafficLightController = new TrafficLightController();
-
-    connect(timer, &QTimer::timeout, this, [this, portPolling]() {
-        portPolling->PollFirstPort();
-        qDebug() << "PollFirstPort call count:" << ++pollCounter;
-    });
-
-    timer->start(100);
-
-    // Timer for activating scanner relay every 500 ms
-    auto *activateScannerTimer = new QTimer(this);
-    connect(activateScannerTimer, &QTimer::timeout, scannerController, &ScannerController::ActivateScannerRelay);
-    activateScannerTimer->start(500);
-
-    // Timer for deactivating scanner relay every 500 ms
-    auto *deactivateScannerTimer = new QTimer(this);
-    connect(deactivateScannerTimer, &QTimer::timeout, scannerController, &ScannerController::DeactivateScannerRelay);
-    deactivateScannerTimer->start(500);
-
-    // Timer for green light every 250 ms
-    auto *greenLightTimer = new QTimer(this);
-    connect(greenLightTimer, &QTimer::timeout, trafficLightController, &TrafficLightController::GreenLight);
-    greenLightTimer->start(250);
+//    auto *portPolling = new PortPolling();
+//    auto *timer = new QTimer(this);
+//    auto *scannerController = new ScannerController();
+//    auto *trafficLightController = new TrafficLightController();
+//
+//    connect(timer, &QTimer::timeout, this, [this, portPolling]() {
+//        portPolling->PollFirstPort();
+//        qDebug() << "PollFirstPort call count:" << ++pollCounter;
+//    });
+//
+//    timer->start(100);
+//
+//    // Timer for activating scanner relay every 500 ms
+//    auto *activateScannerTimer = new QTimer(this);
+//    connect(activateScannerTimer, &QTimer::timeout, scannerController, &ScannerController::ActivateScannerRelay);
+//    activateScannerTimer->start(500);
+//
+//    // Timer for deactivating scanner relay every 500 ms
+//    auto *deactivateScannerTimer = new QTimer(this);
+//    connect(deactivateScannerTimer, &QTimer::timeout, scannerController, &ScannerController::DeactivateScannerRelay);
+//    deactivateScannerTimer->start(500);
+//
+//    // Timer for green light every 250 ms
+//    auto *greenLightTimer = new QTimer(this);
+//    connect(greenLightTimer, &QTimer::timeout, trafficLightController, &TrafficLightController::GreenLight);
+//    greenLightTimer->start(250);
 }
